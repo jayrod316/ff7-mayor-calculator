c83b1de330b6d998da6a10611f047ddfe03c9c15
Jared Vizzi <jared.vizzi@gmail.com>
2021-03-06 16:00:28 -0500

    Added PC passwords, removed button, fixed bugs.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-This is my first little project. It is a simple calculator that predicts the Shinra mayor's password in the Shinra Tower in Final Fantasy VII. This is certainly not a necessary tool, as it is very easy to do the math in your head. This project is mostly just to help me understand and learn a few things about how to put a webpage with some JavaScript together.
+This is my first little project. It is a simple calculator that predicts the Shinra mayor's password in the Shinra Tower in Final Fantasy VII. This is certainly not a necessary tool, as it is very easy to do the math in your head. This project is mostly just to help me understand and learn a few things about how to put a webpage with some JavaScript together. 
+
+The main thing I learned on this project was how to call a JS function by clicking a button or releasing a key.
+
+v1.0
+    -type the IGT seconds and click the button to see the answer
+
+v2.0 
+    -fixed errors from entering floats and negative numbers 
+    -added passwords for the PC version
+    -removed button so the user can simply type and see the answer
--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function getInputValue() {
 
     let seconds = document.getElementById("igt").value;
 
-    console.log(seconds)
-
     let passwords = [
         "Option 5 - MAKO",
         "Option 2 - KING",
@@ -11,22 +9,32 @@ function getInputValue() {
         "Option 4 - BOMB"
     ]
 
-    if (seconds <= 60) {
-        function find() {
-            return seconds % 4;
+    if (seconds <= 60 && seconds >= 0) {
+        function find(seconds) {
+            return key = Math.trunc(seconds % 4);
         }
 
-        let key = find(seconds)
+        key = find(seconds)
 
-        let solution = passwords[key]
+        let psxsolution = passwords[key]
 
-        console.log(solution)
+        if (key < 2) {
+            pcsolution = passwords[key + 2]
+        }
 
-        document.getElementById("solution").innerHTML = solution;
+        else if (key >= 2) {
+            pcsolution = passwords[key - 2]
+        }
+
+        document.getElementById("psxsolution").innerHTML = "PSX: " + psxsolution;
+        document.getElementById("pcsolution").innerHTML = "PC: " + pcsolution;
     }
 
     else {
-        document.getElementById("solution").innerHTML = "Sorry! You must enter an integer between 0 and 60!"
+        document.getElementById("psxsolution").innerHTML = "Sorry! You must enter an integer between 0 and 60."
+        document.getElementById("pcsolution").innerHTML = "Sorry! You must enter an integer between 0 and 60."
+
     }
 
 }
+
